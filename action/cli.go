63740307b3deb1bc10f56ca0d329bb51ee9660a5
komisan19 <komiyama6219@gmail.com>
2021-03-11 04:58:56 +0000

    feat: Add cookbook

--- action/cli.go
+++ action/cli.go
@@ -1,19 +1,33 @@
 package action
 
 import (
+	"fmt"
 	"log"
 	"os"
-  "fmt"
 
 	"github.com/urfave/cli"
 )
 
-func Init(c *cli.Context) {
+func createDockerfile() {
   df, err := os.Create("Dockerfile")
   if err != nil {
-    log.Fatal("Dockerfile is already")
+    log.Fatal(err)
   }
   defer df.Close()
-  fmt.Println("Dockerfile is creaed")
 }
 
+func Init(c *cli.Context) {
+  createDockerfile()
+  fmt.Println("Dockerfile is already")
+}
+
+func Create(c *cli.Context) {
+  rd, err := os.ReadFile("../cookbook/go-dockerfile")
+  if err != nil {
+    log.Fatal(err)
+  }
+  err = os.WriteFile("Dockerfile", rd, 0755)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
