fe8492a233081f98dbd08d4b3dd160fbbf1d12c6
komisan19 <komiyama6219@gmail.com>
2021-03-14 03:47:53 +0000

    add support language

--- memu/menu.go
+++ memu/menu.go
@@ -6,29 +6,29 @@ import (
 )
 
 func AppConfig() *cli.App {
-  app := cli.NewApp()
-  config(app)
-  app.Commands = commands()
-  return app
+	app := cli.NewApp()
+	config(app)
+	app.Commands = commands()
+	return app
 }
 
-func config(app *cli.App){
-  app.Name = "kuzira"
-  app.Usage = "kuzira can automatic creation Dockerfile"
-  app.Version = "0.0.1"
+func config(app *cli.App) {
+	app.Name = "kuzira"
+	app.Usage = "kuzira can automatic creation Dockerfile"
+	app.Version = "0.0.1"
 }
 
-func commands() []cli.Command{
-  return []cli.Command{
-    {
-      Name: "init",
-      Usage: "create on empty Dockerfile",
-      Action: action.Init,
-    },
-    {
-      Name: "create",
-      Usage: "create Dockerfile for each language",
-      Action: action.Create,
-    },
-  }
+func commands() []cli.Command {
+	return []cli.Command{
+		{
+			Name:   "init",
+			Usage:  "create on empty Dockerfile",
+			Action: action.Init,
+		},
+		{
+			Name:   "create",
+			Usage:  "create Dockerfile for each language",
+			Action: action.Create,
+		},
+	}
 }
